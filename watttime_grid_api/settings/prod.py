b9fbd45cadf90503da4ec4ac7fd8571c9a4a50a5
anna <annarschneider@gmail.com>
2014-02-12 11:54:36 -0800

    remove secret key

--- watttime_grid_api/settings/prod.py
+++ watttime_grid_api/settings/prod.py
@@ -127,7 +127,7 @@ COMPRESS_JS_FILTERS += [
 
 ########## SECRET CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = environ.get('SECRET_KEY', SECRET_KEY)
+#SECRET_KEY = environ.get('SECRET_KEY', SECRET_KEY)
 ########## END SECRET CONFIGURATION
 
 ########## ALLOWED HOSTS CONFIGURATION
