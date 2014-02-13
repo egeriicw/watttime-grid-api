b5ad1a11391ab32c1d98680ca4c23c2d8431e34c
anna <annarschneider@gmail.com>
2014-02-12 16:35:05 -0800

    fix dj-static, no static configs in prod

--- watttime_grid_api/settings/common.py
+++ watttime_grid_api/settings/common.py
@@ -90,11 +90,16 @@ MEDIA_URL = '/media/'
 
 
 ########## STATIC FILE CONFIGURATION
+# See: https://github.com/kennethreitz/dj-static
+
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = normpath(join(DJANGO_ROOT, 'static'))
+#STATIC_ROOT = normpath(join(DJANGO_ROOT, 'static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
+
+# INSTEAD, use dj-static
+STATIC_ROOT = 'static'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 #STATICFILES_DIRS = (
@@ -103,11 +108,11 @@ STATIC_URL = '/static/'
 #)
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#STATICFILES_FINDERS = (
+#    'django.contrib.staticfiles.finders.FileSystemFinder',
+#    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
   #  'compressor.finders.CompressorFinder',
-)
+#)
 ########## END STATIC FILE CONFIGURATION
 
 
