505d0b5155b343ebcb7a9755b478a71b5c9c33ae
anna <annarschneider@gmail.com>
2014-02-20 11:17:21 -0800

    add options to update_generation

--- watttime_grid_api/apps/genmix/management/commands/update_generation.py
+++ watttime_grid_api/apps/genmix/management/commands/update_generation.py
@@ -3,17 +3,45 @@ from apps.gridentities.models import BalancingAuthority, FuelType
 from apps.griddata.models import DataPoint, DataSeries
 from apps.clients import client_factory
 from apps.genmix.models import Generation
+from dateutil.parser import parse as dateutil_parse
+import pytz
+from optparse import make_option
 
 
 class Command(BaseCommand):
-    args = 'BA_NAME'
+    args = 'BA_NAME [--dates START_AT END_AT] [--market MARKET]'
     help = 'Add new generation data to the database from the given balancing authority.'
-
+    option_list = BaseCommand.option_list + (
+        make_option('--dates',
+            action='store',
+            nargs=2,
+            help='Start and end dates for obtaining data. If not provided, get only the most recent data point.'),
+        make_option('--market',
+            action='store',
+            nargs=1,
+            help='Market type',
+            default=DataPoint.RTHR,
+            choices=[x[0] for x in DataPoint.MARKET_CHOICES]),
+        )
+        
     def handle(self, ba_name, **options):
+        # parse args
+        try:
+            start_at = pytz.utc.localize(dateutil_parse(options['dates'][0]))
+            end_at = pytz.utc.localize(dateutil_parse(options['dates'][1]))
+            latest = False
+            self.stdout.write('Getting data between %s and %s...' % (start_at, end_at))
+        except TypeError: # dates arg not provided
+            latest = True
+            start_at, end_at = None, None
+            self.stdout.write('Getting the latest data...')
+            
         # get data
         c = client_factory(ba_name)
-        data = c.get_generation(latest=True)
-
+        data = c.get_generation(latest=latest, start_at=start_at, end_at=end_at,
+                                market=options['market'])
+        self.stdout.write('Got the data, inserting...')
+        
         # process data
         n_new_dps = 0
         n_new_gens = 0
