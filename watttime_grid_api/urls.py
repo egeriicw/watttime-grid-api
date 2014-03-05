544f2f5a6c1e3a8343bf95430acd7eb1e735a08a
anna <annarschneider@gmail.com>
2014-03-04 17:03:10 -0800

    first pass at landing page docs

--- watttime_grid_api/urls.py
+++ watttime_grid_api/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.gis import admin
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
@@ -10,6 +11,13 @@ urlpatterns = patterns('',
     # Admin panel and documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # home
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+
+    # api
     url(r'^api/v1/', include('apps.api.urls')),
+
+    # api docs
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
