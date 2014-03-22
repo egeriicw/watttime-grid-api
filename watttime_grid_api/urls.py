83fc3354877f50476b120f0c90249fadc680ab57
anna <annarschneider@gmail.com>
2014-03-22 15:41:44 -0700

    fix home url (was breaking logout redirect)

--- watttime_grid_api/urls.py
+++ watttime_grid_api/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     url(r'^admin_tools/', include('admin_tools.urls')),
 
     # home
-    url(r'^[/]$', TemplateView.as_view(template_name="index.html"), name='home'),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
     url(r'^contact[/]$', TemplateView.as_view(template_name="contact.html"), name='contact'),
     url(r'^dashboard[/]$', TemplateView.as_view(template_name="dashboard.html"), name='dashboard'),
     
