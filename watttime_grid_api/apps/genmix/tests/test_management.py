03b551bff3697f70740d33b4d23118d291aec550
anna <annarschneider@gmail.com>
2014-03-28 20:06:50 -0700

    management task tests better cover integration with pyiso

--- watttime_grid_api/apps/genmix/tests/test_management.py
+++ watttime_grid_api/apps/genmix/tests/test_management.py
@@ -1,6 +1,8 @@
 from django.core.management import call_command
 from django.test import TestCase
 from apps.genmix.models import Generation
+from apps.gridentities.models import BalancingAuthority
+from datetime import datetime
 
 
 class TestCommand(TestCase):
@@ -12,11 +14,24 @@ class TestCommand(TestCase):
         self.assertGreater(Generation.objects.filter(mix__ba__abbrev=ba_name).count(), 0)
             
     def test_no_kwargs(self):
-        for ba_name in ['CAISO', 'ISONE', 'PJM', 'SPP', 'MISO']:
+        # get non-special-case ISO names
+        special_cases = ['BPA', 'NYISO', 'ERCOT']
+        ba_names = BalancingAuthority.objects.exclude(abbrev__in=special_cases).values_list('abbrev', flat=True)
+
+        for ba_name in ba_names:
             self._run_test(ba_name)
         
+    def test_failing_BPA_wo_kwargs(self):
         self.assertRaises(ValueError, self._run_test, 'BPA')
         
-    def test_market_kwargs(self):
+    def test_BPA_requires_kwargs(self):
         self._run_test('BPA', market='RT5M')
-        
+
+    def test_NYISO_fails(self):
+        self.assertRaises(ValueError, self._run_test, 'NYISO')
+
+    def test_ERCOT_fails_before_min33(self):
+        if datetime.now().minute > 33:
+            self._run_test('ERCOT')
+        else:
+            self.assertRaises(AssertionError, self._run_test, 'ERCOT')
