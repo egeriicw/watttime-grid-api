ae0d551977b24f61dd1ee394fab18a9a3f14403c
anna <annarschneider@gmail.com>
2014-04-06 12:36:49 -0700

    add Siler-Evans MOER to admin

--- watttime_grid_api/apps/marginal/admin.py
+++ watttime_grid_api/apps/marginal/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from apps.marginal.models import SilerEvansModel
+from apps.marginal.models import SilerEvansModel, SilerEvansMOER
 
 admin.site.register(SilerEvansModel)
+admin.site.register(SilerEvansMOER)
