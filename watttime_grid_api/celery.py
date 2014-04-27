5b1a82ea93ee8371f7704eda78530a568abeaeb7
anna <annarschneider@gmail.com>
2014-04-26 23:25:02 -0700

    move celery file back to top, and put schedule in separate file

---
+++ watttime_grid_api/celery.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+from django.conf import settings
+
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watttime_grid_api.settings.dev')
+app = Celery('watttime_grid_api')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+
+# autodiscover tasks in any app
+app.autodiscover_tasks(settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+
+# set schedule
+from apps.etl.schedule import schedule
+app.conf.CELERYBEAT_SCHEDULE = schedule
