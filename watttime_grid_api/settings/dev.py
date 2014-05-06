0115d207143f82284b7df868fe26cb92a0f98d07
anna <annarschneider@gmail.com>
2014-05-05 22:20:38 -0700

    upgrade django debug toolbar

--- watttime_grid_api/settings/dev.py
+++ watttime_grid_api/settings/dev.py
@@ -64,7 +64,4 @@ MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
 ########## END TOOLBAR CONFIGURATION
