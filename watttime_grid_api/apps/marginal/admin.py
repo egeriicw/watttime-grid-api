b2785b2f1c57336e30571cbab9fe664d7121a9dd
anna <annarschneider@gmail.com>
2014-05-09 15:10:08 -0700

    nicer admin for MOER

--- watttime_grid_api/apps/marginal/admin.py
+++ watttime_grid_api/apps/marginal/admin.py
@@ -9,7 +9,10 @@ class StructuralModelSetAdmin(admin.ModelAdmin):
 	inlines = [StructuralModelInline]
 
 
+class MOERAdmin(admin.ModelAdmin):
+	list_display = ('id', 'structural_model', 'dp', 'value', 'units')
+
 admin.site.register(StructuralModelSet, StructuralModelSetAdmin)
 admin.site.register(SimpleStructuralModel)
 admin.site.register(MOERAlgorithm)
-admin.site.register(MOER)
+admin.site.register(MOER, MOERAdmin)
