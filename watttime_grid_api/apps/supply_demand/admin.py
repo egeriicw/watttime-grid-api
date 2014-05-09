29452432e96598c33c73e058d1dde06d0919c905
anna <annarschneider@gmail.com>
2014-05-09 15:24:20 -0700

    nicer admin for other supply_demand models

--- watttime_grid_api/apps/supply_demand/admin.py
+++ watttime_grid_api/apps/supply_demand/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from apps.supply_demand.models import Load, TieFlow, Generation
 
-admin.site.register(Load)
-admin.site.register(TieFlow)
-admin.site.register(Generation)
+class LoadAdmin(admin.ModelAdmin):
+	list_display = ('id', 'dp', 'value', 'units')
+
+
+class TieFlowAdmin(admin.ModelAdmin):
+	list_display = ('id', 'dp_source', 'dp_dest', 'value', 'units')
+
+
+class GenerationAdmin(admin.ModelAdmin):
+	list_display = ('id', 'mix', 'fuel', 'gen_MW')
+
+
+admin.site.register(Load, LoadAdmin)
+admin.site.register(TieFlow, TieFlowAdmin)
+admin.site.register(Generation, GenerationAdmin)
