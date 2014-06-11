384ed6e43c3b2027056f64c511b99e63abcc3d8c
anna <annarschneider@gmail.com>
2014-06-10 23:54:11 -0700

    fix whitespace

--- watttime_grid_api/apps/api/permissions.py
+++ watttime_grid_api/apps/api/permissions.py
@@ -2,11 +2,11 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class IsInGroup(IsAuthenticated):
-	"""
-	Allow access to authenticated users in a group
-	"""
-	GROUP = 'team'
+    """
+    Allow access to authenticated users in a group
+    """
+    GROUP = 'team'
 
-	def has_permission(self, request, view):
-		if super(IsInGroup, self).has_permission(request, view):
-			return request.user.groups.all().filter(name=self.GROUP).exists()
+    def has_permission(self, request, view):
+        if super(IsInGroup, self).has_permission(request, view):
+            return request.user.groups.all().filter(name=self.GROUP).exists()
